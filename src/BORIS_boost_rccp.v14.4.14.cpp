ca24b93ed5387b507e203520c16f8827d71c5b1f
Shandu Mulaudzi <shandu.mulaudzi@gmail.com>
2021-11-22 11:39:21 -0500

    Updated debugging statements

--- src/BORIS_boost_rccp.v14.4.14.cpp
+++ src/BORIS_boost_rccp.v14.4.14.cpp
@@ -12890,7 +12890,7 @@ Rcpp::List infer_cpp() {
 	ind_n_base_part = para_priorsetc.ind_n_base_part;// =1 if the seq data is partial
 	n_base_part = para_priorsetc.n_base_part; // the partial length used if ind_n_base_part =1
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 	para_key_init para_init;
 	IO_parakeyinit(para_init);
@@ -12898,7 +12898,7 @@ Rcpp::List infer_cpp() {
 	para_scaling_factors para_scalingfactors;
 	IO_parascalingfactors(para_scalingfactors);
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 	para_aux para_other;
 	IO_para_aux(para_other);  //Importing parameters
@@ -12909,7 +12909,7 @@ Rcpp::List infer_cpp() {
 	opt_mov = para_other.opt_mov;
 	debug = para_other.debug;
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 	epi_struct epi_final;
 	nt_struct nt_data;
@@ -12920,7 +12920,7 @@ Rcpp::List infer_cpp() {
 	vector<int> con_seq, con_seq_estm;
 	IO_data(para_other, coordinate, epi_final, nt_data, index, con_seq_estm, seeds, moves); //Importing  data
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 
 	// set a universal seed
@@ -12952,7 +12952,7 @@ Rcpp::List infer_cpp() {
 
 	rng_type rng(seed); //set a universal seed
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 	vec2int sample_data; // 2-d vector contains the sampled sequences; non-sampled premises would have unexpected values
 	sample_data.resize(NLIMIT);
@@ -12984,7 +12984,7 @@ Rcpp::List infer_cpp() {
 	}// end while for getline
 	myfile_in.close();
 
-  cerr << "error above line " << __LINE__ << "\n" // ###
+  cerr << "error above line " << __LINE__ << "\n"; // ###
 
 	// upload the true infected sources vector
 	vector<int> atab_from; // vector to hold true sources (from accuracy table comparison file)
